59f23a047213614fab49269e22baaa2e639fd083
Tymophiy Radchenko <timaradchen@gmail.com>
2026-07-20 09:12:33 +0200

    Added db connection

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,11 +6,15 @@ use std::io::ErrorKind::UnexpectedEof;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
 use tokio::sync::broadcast::Sender;
+use std::time::Duration;
+use sqlx::{self, mysql::{MySqlPoolOptions, MySqlPool}};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8080").await?;
     let (tx, _rx) = tokio::sync::broadcast::channel::<ChatMessage>(1024);
+    let db_url = "mysql://root:Tima1405pereviz@localhost:3306/chat_db";
+    let db_pool = estabilish_connection(db_url).await.unwrap();
     println!("Listening on 127.0.0.1:8080");
 
     loop {
@@ -36,6 +40,16 @@ async fn main() -> io::Result<()> {
             routes::handle_routes(stream, &buffer[0..n], tx_cloned).await;
         });
     }
+}
+
+pub async fn estabilish_connection(db_url: &str) -> anyhow::Result<MySqlPool> {
+    let pool = MySqlPoolOptions::new()
+        .max_connections(50)
+        .acquire_timeout(Duration::from_secs(3))
+        .idle_timeout(Duration::from_secs(10))
+        .connect(db_url)
+        .await?;
+    Ok(pool)
 }
 
 
